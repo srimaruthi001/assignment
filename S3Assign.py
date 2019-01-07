c07a3d0608916067596c509ef96b8b42eb2742b3
srimaruthi001 <32559535+srimaruthi001@users.noreply.github.com>
2019-01-06 23:34:31 -0500

    Add s3 list logic

--- S3Assign.py
+++ S3Assign.py
@@ -1,3 +1,7 @@
 import boto3
+from boto3 import client
 s3=boto3.resource('s3')
+resp=client('s3')
 s3.meta.client.upload_file('file_path/Assignment.txt','wiproinpimages','assgnmt.txt')
+for obj in resp.list_objects(Bucet='wiproinpimages')['Contents']:
+    print(obj['Key'])
